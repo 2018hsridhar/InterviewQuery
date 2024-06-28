783e84e29c42ce7625aa63b4404e6e4df590c192
Hari Sridhar <hari.h.sridhar@gmail.com>
2024-06-28 07:30:15 -0700

    Update topThreeSalaries.py
    The concept of mental freshness -> please just sleep, keep a basic routine, and it will make more sense the next day. 
    TRUST YOUR MIND!!

--- topThreeSalaries.py
+++ topThreeSalaries.py
@@ -1,27 +1,25 @@
 '''
-URL := https://www.interviewquery.com/questions/top-three-salaries
-3 highest emp salaries : by department
-top 2 or top 1 -> if not all top 3 ( shouldn't the .head(n) func still work anyways ) 
-first naem concat last naem thing too
+Assumee each dept : @ least 1 emp ( no left join complexity )
+Ask table assumptinos one can make
 
-Each dept has @ least 1 employee -> can do inner join 
+https://www.interviewquery.com/questions/top-three-salaries
 
-Return back to this later :-)
+[dept_id,salary,name] ( asc,desc) order
+groupby(deptId).head(3) reasoning
+ exec reordering later on
+
+ Salaries could repeat -> assume not unique
 '''
 import pandas as pd
 
 def top_three_salaries(departments: pd.DataFrame, employees: pd.DataFrame):
-    departments.rename(columns={'name':'department_name'},inplace=True)
-    # print(departments)
-    targetCols = ['employee_name','department_name','salary']
     employees['employee_name'] = employees['first_name'] + ' ' + employees['last_name']
-    edt = pd.merge(employees,departments,left_on='department_id',right_on='id',how='inner')
-    dimReduced = edt[targetCols]
-    # print(dimReduced)
-    top3Table = dimReduced.sort_values(by=['department_name','employee_name','salary'],ascending=False).groupby('department_name').head(3)
-    # top3Table = top3Table.sort_values(by=['department_name'],ascending=[True])
-    top3Table['lowercase'] = top3Table['department_name'].str.lower()
-    top3Table = top3Table.sort_values(by=['lowercase','salary'],ascending=[True,False])
-    del top3Table['lowercase']
-    # hard to retrieve original columnar information back again
-    return top3Table
+    employees.drop(columns=['first_name','last_name'],inplace=True)
+    employees.sort_values(by=['department_id','salary'],axis=0,ascending=[True,False],inplace=True)
+    report = employees.groupby('department_id').head(3)
+    report = pd.merge(report,departments,left_on='department_id',right_on='id',how='inner')
+    report.drop(columns=['id_x','department_id','id_y'],inplace=True)
+    report.rename(columns={'name':'department_name'},inplace=True)
+    reportOrder = ['employee_name','department_name','salary']
+    report = report[reportOrder]
+    return report
