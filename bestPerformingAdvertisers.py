78a648e33d9a16849a46e2dcc7f6b33fc1f94337
Hari Sridhar <hari.h.sridhar@gmail.com>
2024-07-03 12:37:18 -0700

    Update bestPerformingAdvertisers.py
    Modularize your code -> it feels less overwhelming to read code that has been modularized, and the longer definition names makes it more clear as to what the original context of our business logic entailed!

--- bestPerformingAdvertisers.py
+++ bestPerformingAdvertisers.py
@@ -14,6 +14,9 @@ Please read and understand the question under ask properly!!!
 
 The three-best performing days -> for those who hit the highest weekly revenues ( of a given week ) !!
 
+Nasty-ass bug :
+    # weeklyRevenue['week'] = weeklyRevenue['transaction_date'].dt.isocalendar().week
+    highestWeeklyRevenue['week'] = highestWeeklyRevenue['transaction_date'].dt.strftime('%U')
 
 '''
 import pandas as pd
@@ -22,34 +25,28 @@ import numpy as np
 # the week in itself is off ( woooh time bug ) !!! 08/01 - 08/07 ( Sunday - Saturday )
 def getHighestWeeklyRevenues2021(advertisers: pd.DataFrame, revenue: pd.DataFrame):
     # STEP [1] Solve for the max weekly revenue
-    highestWeeklyRevenue = revenue[['advertiser_id','transaction_date','amount']]
+    filterCols = ['advertiser_id','transaction_date','amount']
+    highestWeeklyRevenue = revenue[filterCols]
     highestWeeklyRevenue['transaction_date'] = pd.to_datetime(highestWeeklyRevenue['transaction_date'])
     highestWeeklyRevenue['week'] = highestWeeklyRevenue['transaction_date'].dt.strftime('%U')
     highestWeeklyRevenue['weeklyAmountByAdvertiser'] = highestWeeklyRevenue.groupby(['advertiser_id','week'])['amount'].transform(np.sum)
-    maxAgg = pd.NamedAgg(column='weeklyAmountByAdvertiser',aggfunc='max')
-    highestWeeklyRevenue2021 = highestWeeklyRevenue.groupby(['week']).agg(weeklyAmountByAdvertiser = maxAgg)
-    maxWeeklyRevenue = highestWeeklyRevenue2021['weeklyAmountByAdvertiser'].max()
-    return maxWeeklyRevenue
+    return highestWeeklyRevenue
 
-def best_performing_advertisers(advertisers: pd.DataFrame, revenue: pd.DataFrame):
-    maxWeeklyRevenue = getHighestWeeklyRevenues2021(advertisers, revenue)
-    
-    weeklyRevenue = revenue[['advertiser_id','transaction_date','amount']]
-    weeklyRevenue['transaction_date'] = pd.to_datetime(weeklyRevenue['transaction_date'])
-    # weeklyRevenue['week'] = weeklyRevenue['transaction_date'].dt.isocalendar().week
-    weeklyRevenue['week'] = weeklyRevenue['transaction_date'].dt.strftime('%U')
-    weeklyRevenue['weeklyAmountByAdvertiser'] = weeklyRevenue.groupby(['advertiser_id','week'])['amount'].transform(np.sum)
-    weeklyRevenue.sort_values(by=['week','amount'],inplace=True)
-
-    # STEP [2] Solve for the advertisers with said max weekly revenues
+def getThreeBestPerfDaysPerAdvertiser(advertisers: pd.DataFrame, weeklyRevenue: pd.DataFrame):
+    maxWeeklyRevenue = weeklyRevenue['weeklyAmountByAdvertiser'].max()
     bestAdvertisers = weeklyRevenue[weeklyRevenue['weeklyAmountByAdvertiser'] == maxWeeklyRevenue]
-    bestAdvertisers = bestAdvertisers['advertiser_id'].unique()
-    advList = list(bestAdvertisers)
+    advList = bestAdvertisers['advertiser_id'].unique()
     bestAdvertisersTrans = weeklyRevenue.loc[weeklyRevenue['advertiser_id'].isin(advList)]
     bestAdvertisersTrans = bestAdvertisersTrans.sort_values(by=['amount'],ascending=[False])
     bestAdvertisersTrans = bestAdvertisersTrans.groupby('advertiser_id').head(3)
-    bestAdvertisersTrans = pd.merge(bestAdvertisersTrans,advertisers,how='inner',left_on='advertiser_id',right_on='advertiser_id')
+    bestAdvertisersTrans = pd.merge(bestAdvertisersTrans,advertisers,how='inner',on='advertiser_id')
     bestAdvertisersTrans.drop(columns=['week','advertiser_id','weeklyAmountByAdvertiser'],inplace=True)
     bestAdvertisersTrans['transaction_date'] = pd.to_datetime(bestAdvertisersTrans['transaction_date']).dt.date
     finalReportCols = ['advertiser_name','transaction_date','amount']
-    return bestAdvertisersTrans[finalReportCols]
+    finalReport = bestAdvertisersTrans[finalReportCols]
+    return finalReport
+
+def best_performing_advertisers(advertisers: pd.DataFrame, revenue: pd.DataFrame):
+    weeklyRevenue = getHighestWeeklyRevenues2021(advertisers, revenue)
+    finalReport = getThreeBestPerfDaysPerAdvertiser(advertisers, weeklyRevenue)
+    return finalReport
